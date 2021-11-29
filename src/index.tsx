3f6339b3a7dd323de9f2a6e107a52ba50096e16f
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-11-29 22:22:35 +0700

    add routing and create initial component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import ApolloClient from "apollo-boost";
 
 import { Home, Host, Listing, Listings, NotFound, User } from "./section";
 import "./styles/index.css";
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/host" component={Host} />
+        <Route exact path="/listings/:location?" component={Listings} />
+        <Route exact path="/listing/:id" component={Listing} />
+        <Route exact path="/user" component={User} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
 
 const client = new ApolloClient({
   uri: "/api",
@@ -13,7 +28,7 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Listings title="TinyHouse Listings" />,
+    <App />
   </ApolloProvider>,
 
   document.getElementById("root")
