1748ca0bc12ce26ebc5ab53f6066b6de56cd6115
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-12-06 19:39:15 +0700

    build Listings Pagination component

---
+++ src/section/Listings/components/ListingsSkeleton/index.tsx
@@ -0,0 +1,33 @@
+import { Card, List, Skeleton } from "antd";
+import React from "react";
+
+export const ListingsSkeleton = () => {
+  const emptyData = [{}, {}, {}, {}, {}, {}, {}, {}];
+
+  return (
+    <div className="listings-skeleton">
+      <Skeleton paragraph={{ rows: 1 }} />
+
+      <List
+        grid={{ gutter: 10, xs: 1, sm: 2, lg: 4 }}
+        dataSource={emptyData}
+        renderItem={() => (
+          <List.Item>
+            <Card
+              className="listings-skeleton__card"
+              cover={
+                <div
+                  style={{
+                    backgroundColor: "#f0f2f3",
+                  }}
+                  className="listings-skeleton__card-cover-img"
+                ></div>
+              }
+              loading
+            />
+          </List.Item>
+        )}
+      ></List>
+    </div>
+  );
+};
