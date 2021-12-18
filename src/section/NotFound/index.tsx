bad2467cebc8b0a906786cf36db7838e5e2e2743
Hafidh Pradipta A <hamstergeek38@gmail.com>
2021-12-18 11:34:25 +0700

    create not found component

--- src/section/NotFound/index.tsx
+++ src/section/NotFound/index.tsx
@@ -1,5 +1,26 @@
+import { Empty, Layout, Typography } from "antd";
 import React from "react";
+import { Link } from "react-router-dom";
+
+const { Content } = Layout;
+const { Text } = Typography;
 
 export const NotFound = () => {
-  return <h2>NotFound</h2>;
+  return (
+    <Content className="not-found">
+      <Empty
+        description={
+          <>
+            <Text className="not-found__description-title">
+              Uh oh! something went wrong
+            </Text>
+            <Text className="not-found__description-subtitle">
+              The page you're looking can't be found
+            </Text>
+          </>
+        }
+      />
+      <Link to="/">Go to Home</Link>
+    </Content>
+  );
 };
